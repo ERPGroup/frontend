edf6483e839d8865fe4972e54fc74ea35e86456d
DefTnT <jimmi2051@gmail.com>
2018-09-24 00:13:58 +0700

    Backup-folder ( folder template )

---
+++ Folder-Theme-Backup/Deal/assets/custom.js
@@ -0,0 +1,364 @@
+//owl2row plugin
+;(function ($, window, document, undefined) {
+    Owl2row = function (scope) {
+        this.owl = scope;
+        this.owl.options = $.extend({}, Owl2row.Defaults, this.owl.options);
+        //link callback events with owl carousel here
+
+        this.handlers = {
+            'initialize.owl.carousel': $.proxy(function (e) {
+                if (this.owl.settings.owl2row) {
+                    this.build2row(this);
+                }
+            }, this)
+        };
+
+        this.owl.$element.on(this.handlers);
+    };
+
+    Owl2row.Defaults = {
+        owl2row: false,
+        owl2rowTarget: 'item',
+        owl2rowContainer: 'owl2row-item',
+        owl2rowDirection: 'utd' // ltr
+    };
+
+    //mehtods:
+    Owl2row.prototype.build2row = function(thisScope){
+    
+        var carousel = $(thisScope.owl.$element);
+        var carouselItems = carousel.find('.' + thisScope.owl.options.owl2rowTarget);
+
+        var aEvenElements = [];
+        var aOddElements = [];
+
+        $.each(carouselItems, function (index, item) {
+            if ( index % 2 === 0 ) {
+                aEvenElements.push(item);
+            } else {
+                aOddElements.push(item);
+            }
+        });
+
+        carousel.empty();
+
+        switch (thisScope.owl.options.owl2rowDirection) {
+            case 'ltr':
+                thisScope.leftToright(thisScope, carousel, carouselItems);
+                break;
+
+            default :
+                thisScope.upTodown(thisScope, aEvenElements, aOddElements, carousel);
+        }
+
+    };
+
+    Owl2row.prototype.leftToright = function(thisScope, carousel, carouselItems){
+
+        var o2wContainerClass = thisScope.owl.options.owl2rowContainer;
+        var owlMargin = thisScope.owl.options.margin;
+
+        var carouselItemsLength = carouselItems.length;
+
+        var firsArr = [];
+        var secondArr = [];
+
+        //console.log(carouselItemsLength);
+
+        if (carouselItemsLength %2 === 1) {
+            carouselItemsLength = ((carouselItemsLength - 1)/2) + 1;
+        } else {
+            carouselItemsLength = carouselItemsLength/2;
+        }
+
+        //console.log(carouselItemsLength);
+
+        $.each(carouselItems, function (index, item) {
+
+
+            if (index < carouselItemsLength) {
+                firsArr.push(item);
+            } else {
+                secondArr.push(item);
+            }
+        });
+
+        $.each(firsArr, function (index, item) {
+            var rowContainer = $('<div class="' + o2wContainerClass + '"/>');
+
+            var firstRowElement = firsArr[index];
+                firstRowElement.style.marginBottom = owlMargin + 'px';
+
+            rowContainer
+                .append(firstRowElement)
+                .append(secondArr[index]);
+
+            carousel.append(rowContainer);
+        });
+
+    };
+
+    Owl2row.prototype.upTodown = function(thisScope, aEvenElements, aOddElements, carousel){
+
+        var o2wContainerClass = thisScope.owl.options.owl2rowContainer;
+        var owlMargin = thisScope.owl.options.margin;
+
+        $.each(aEvenElements, function (index, item) {
+
+            var rowContainer = $('<div class="' + o2wContainerClass + '"/>');
+            var evenElement = aEvenElements[index];
+
+            evenElement.style.marginBottom = owlMargin + 'px';
+
+            rowContainer
+                .append(evenElement)
+                .append(aOddElements[index]);
+
+            carousel.append(rowContainer);
+        });
+    };
+
+    /**
+     * Destroys the plugin.
+     */
+    Owl2row.prototype.destroy = function() {
+        var handler, property;
+
+        for (handler in this.handlers) {
+            this.owl.dom.$el.off(handler, this.handlers[handler]);
+        }
+        for (property in Object.getOwnPropertyNames(this)) {
+            typeof this[property] !== 'function' && (this[property] = null);
+        }
+    };
+
+    $.fn.owlCarousel.Constructor.Plugins['owl2row'] = Owl2row;
+})( window.Zepto || window.jQuery, window,  document );
+//end of owl2row plugin
+
+
+$(document).ready(function () {
+    $('#click_drop_menu').click(function (e) {
+        e.stopPropagation();
+        $('#sub-menu').toggleClass('show-menu');
+    });
+    $('#click_drop_menu_fixed').click(function (e) {
+        e.stopPropagation();
+        $('#sub-menu').toggleClass('show-menu');
+    });
+    $('#sub-menu').click(function(e){
+        e.stopPropagation();
+    });
+    
+    $('body,html').click(function(e){
+       $('#sub-menu').removeClass('show-menu');
+    });
+    $('#click_exit_menu').click(function(){
+        $('#sub-menu').removeClass('show-menu');
+    })
+});
+$('.owl-slider').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        767: {
+            items: 1
+        }
+        ,
+        992: {
+            items: 1
+        },
+        1200: {
+            items: 1
+        }
+    }
+})
+$('.owl-product-hot').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 2
+        },
+        767: {
+            items: 2
+        }
+        ,
+        992: {
+            items: 3
+        },
+        1200: {
+            items: 4
+        }
+    }
+})
+$('.owl-product-buy-more').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 2
+        },
+        767: {
+            items: 2
+        }
+        ,
+        992: {
+            items: 3
+        },
+        1200: {
+            items: 4
+        }
+    }
+})
+$('.owl-product-sales').owlCarousel({
+    loop: false,
+    margin: 10,
+    nav: true,
+    owl2row: true,
+    responsive: {
+        0: {
+            items: 2
+        },
+        767: {
+            items: 2
+        }
+        ,
+        992: {
+            items: 3
+        },
+        1200: {
+            items: 4
+        }
+    }
+})
+$('.owl-product-phone').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 2
+        },
+        767: {
+            items: 2
+        }
+        ,
+        992: {
+            items: 3
+        },
+        1200: {
+            items: 4
+        }
+    }
+})
+$('.owl-product-ipad').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 2
+        },
+        767: {
+            items: 2
+        }
+        ,
+        992: {
+            items: 3
+        },
+        1200: {
+            items: 4
+        }
+    }
+})
+$('.owl-product-laptop').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 2
+        },
+        767: {
+            items: 2
+        }
+        ,
+        992: {
+            items: 3
+        },
+        1200: {
+            items: 4
+        }
+    }
+})
+$('.owl-brand').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 2
+        },
+        767: {
+            items: 3
+        }
+        ,
+        992: {
+            items: 5
+        },
+        1200: {
+            items: 7
+        }
+    }
+})
+window.onscroll = function() {scrollFunction();fixMenuMobile();};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("btnTop").style.display = "block";
+    } else {
+        document.getElementById("btnTop").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+// Get the header
+var header = document.getElementById("fixed_menu_top");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function fixMenuMobile() {
+    if (window.pageYOffset > 200) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+
+function clickShowFooter(a)
+{
+    var x = document.getElementById(a);
+    if(x.style.display == "block")
+    {
+        x.style.display = "none";
+    }
+    else
+    {
+        x.style.display = "block";
+    }
+}
