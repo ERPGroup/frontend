b9b006c19330d2f164462ffeefa6fc429ae695f0
DefTnT <jimmi2051@gmail.com>
2018-09-24 20:32:27 +0700

    Fixed

--- Folder-Theme-Backup/Deal/assets/custom.js
+++ Folder-Theme-Backup/Deal/assets/custom.js
@@ -161,6 +161,8 @@ $('.owl-slider').owlCarousel({
     loop: true,
     margin: 10,
     nav: true,
+    autoplay:true,
+    autoplayTimeout:5000,
     responsive: {
         0: {
             items: 1
